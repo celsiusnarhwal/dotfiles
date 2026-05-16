7029a298137511c8d0f8ba92a7155d4f85b3d450
celsius narhwal <hello@celsiusnarhwal.dev>
2026-05-16 08:50:13 -0400

    Add .config/zsh/zshrc.d/local/Sakura/01_path.zsh Add .config/zsh/zshrc.d/local/Sakura/03_aliases.zsh Add .config/zsh/zshrc.d/local/Sakura/04_functions.zsh Add .config/zsh/zshrc.d/local/Sakura/02_vars.zsh Add .config/zsh/zshrc.d/local/Sakura/05_misc.zsh

---
+++ private_dot_config/private_zsh/zshrc.d/local/Sakura/03_aliases.zsh
@@ -0,0 +1,2 @@
+alias sba="spicetify backup apply"
+alias keka="/Applications/Keka.app/Contents/MacOS/Keka --cli"
---
+++ private_dot_config/private_zsh/zshrc.d/local/Sakura/04_functions.zsh
@@ -0,0 +1,3 @@
+function crx {
+	"/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --pack-extension=$@ --pack-extension-key=$@.pem
+}

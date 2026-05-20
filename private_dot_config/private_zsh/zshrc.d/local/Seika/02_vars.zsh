5dcd52ad79ced3384d6f991850ade0677cc866fb
celsius narhwal <hello@celsiusnarhwal.dev>
2026-05-19 21:38:48 -0400

    Update .config/zsh/zshrc.d/local/Seika/02_vars.zsh

--- private_dot_config/private_zsh/zshrc.d/local/Seika/02_vars.zsh
+++ private_dot_config/private_zsh/zshrc.d/local/Seika/02_vars.zsh
@@ -2,3 +2,4 @@ export MEDIA_DIR=/media/celsiusnarhwal/Seikadrive
 export CONFIG_DIR=/media/celsiusnarhwal/Seikafile
 export VAULT_DIR=/media/celsiusnarhwal/Seikavault
 export COMPOSE_FILE=${CONFIG_DIR}/docker-compose.yml
+export GCM_CREDENTIAL_STORE=gpg
